1f17498eca00199cc25ef092e755c2017b5b04ca
박수빈 <jrkd2165@gmail.com>
2023-10-19 17:29:31 +0900

    :sparkles: Initial commit

---
+++ springFcmTest/src/main/java/com/galaxy/springFcmTest/SpringFcmTestApplication.java
@@ -0,0 +1,13 @@
+package com.galaxy.springFcmTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFcmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFcmTestApplication.class, args);
+	}
+
+}
